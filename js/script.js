548867acfcde5fc094bf508ed56e37d287b74d59
Łukasz Spalczyński <spali89@gmail.com>
2019-07-03 20:40:02 +0200

    Add loop for remove active class from article

--- js/script.js
+++ js/script.js
@@ -6,6 +6,19 @@ function titleClickHandler(event){
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+
+  const activeArticles = document.querySelectorAll('.post');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
+
+
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
